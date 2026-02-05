9bcea4654fa94d027252b77d6ab54f50d7dd7f4b
Sabina Abubakirova <bisb22-30@NTDOM.LOCAL>
2026-02-05 09:58:17 +0500

    lab_4

--- lab_3/flutter_application_1/lib/main.dart
+++ lab_3/flutter_application_1/lib/main.dart
@@ -1,56 +1,84 @@
 import 'package:flutter/material.dart';
-void main() {
 
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
-  get body => null;
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
-      title: "Welcome",
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 179, 58, 183)),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: "Home page"),
-      
-      
-     
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
- 
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My app'),
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
       ),
-      body: const Center(
-        child: Text(
-          'Hello to MY APP'
-        ),
-      ),
-    );
+      body: Column(
+        
+        children: <Widget>[
+          SizedBox(height: 30,),
+          Stack(
+          children: [
+            SizedBox(height: 30,),
+            Image.asset("assets/images/image1.jpg", height: 300,width: 200,fit: BoxFit.contain,),
+            Container(height: 320, width: 220, color: Colors.black.withOpacity(0.5),),
+            Positioned.fill(child: Center(
+              child: Text("Welcome to Flutter", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center, ),
+            ))
+          ]
+          ),
+          SizedBox(height: 30,),
 
+           Column(
+          children: [
+            SizedBox(height: 30,),
+            Image.asset("assets/images/image2.jpg",height: 200,width: 300, fit: BoxFit.contain,),
+
+            SizedBox(height: 20,),
+
+            SizedBox(width: 200, height: 50,
+            child: ElevatedButton(
+              onPressed: () {}, child: Text("Show SnackBar", style: TextStyle(fontSize: 16, color: Colors.white),))
+            ),
+         
+         
+          ],
+
+          ),
+
+
+
+
+
+
+        ],
+      ),
+      
+    );
   }
 }
